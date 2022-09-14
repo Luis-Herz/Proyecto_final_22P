9a0f7ac9c7308446fbff0848829e5467b82c4f70
Luis-Herz <2153068030@cua.uam.mx>
2022-09-14 17:52:22 -0500

    Avance del proyecto recibir frase entera

--- PF_Ahorcado.asm
+++ PF_Ahorcado.asm
@@ -8,5 +8,36 @@
 # El juego termina si el usuario adivina la frase, se rinde o se le acaban los intentos.
 
 .data
+	peticion: .asciiz "Escribe una letra: \n"
+	intentos: .asciiz "Tienes este numero de intentos: "
+	error: .asciiz "Esta letra no pertenece a la frase \n"
+	resultado: .asciiz "Esta es la frase: "
+	enter: .asciiz "\n"
+	frase1: .asciiz "Hola mundo"
+	frase2: .space 20 
 
 .text
+
+	#li $v0, 4
+	#la $a0, peticion
+	#syscall 
+	
+	la $a0, frase2			# Cargamos la direccion de la frase en $a0, al mismo tiempo la frase del usuario se guardara en frase2
+	li $a1, 13				# La frase del usuario solo podra tener 12 letras (Incluidos los espacios)
+	li $v0, 8
+	syscall
+	
+	li $v0, 4
+	la $a0, enter
+	syscall 
+	
+	li $v0, 4
+	la $a0, resultado
+	syscall 
+	
+	li $v0, 4
+	la $a0, frase2
+	syscall 
+	
+	li $v0, 10
+	syscall 
